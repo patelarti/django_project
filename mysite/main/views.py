65f8fe1a25f9423dd3b6d4fef902cbb3e02bee8f
patelarti <patelarti91@gmail.com>
2024-12-25 08:53:05 -0600

    added custom templates, used jinja syntax and modified urls and views accordingly.

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ToDoList, Item
+
+
 # Create your views here.
-def index(response,id):
+def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    item = ls.item_set.get(id=1)
-    return HttpResponse("<h1>%s</h1></br><p>%s</p>" %(ls.name, item.text))
+    return render(response, "main/list.html", {"ls":ls})
+
+def home(response):
+    return render(response, "main/home.html", {})
